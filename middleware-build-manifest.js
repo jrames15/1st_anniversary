f25b9a75fbf99f668b8c5b9e05d2e050579c1e9d
jrames15 <jamesrheinm@gmail.com>
2026-07-04 23:23:17 +0800

    Add files via upload

---
+++ middleware-build-manifest.js
@@ -0,0 +1,20 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": []
+  },
+  "devFiles": [],
+  "polyfillFiles": [
+    "static/chunks/0cz1d0mv5g_q7.js"
+  ],
+  "lowPriorityFiles": [
+    "static/VtX4wLnNHcgLeG6BKnxG6/_buildManifest.js",
+    "static/VtX4wLnNHcgLeG6BKnxG6/_ssgManifest.js",
+    "static/VtX4wLnNHcgLeG6BKnxG6/_clientMiddlewareManifest.js"
+  ],
+  "rootMainFiles": [
+    "static/chunks/3aojk-ow77wd2.js",
+    "static/chunks/0cbr67yte101v.js",
+    "static/chunks/3peubv2924kx4.js",
+    "static/chunks/turbopack-3fhcwxpteg27u.js"
+  ]
+};
